6b2c23c8fa123f4a551c585cb49592c6fa532e27
Jhon Giraldo <jhon.giraldo@pragma.com.co>
2022-09-20 15:47:07 -0500

    Prueba commit con VS

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,45 +6,37 @@ import 'package:peliculas/search/search_delegate.dart';
 import 'package:peliculas/providers/movies_provider.dart';
 import 'package:peliculas/widgets/widgets.dart';
 
-
-
 class HomeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     final moviesProvider = Provider.of<MoviesProvider>(context);
 
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Películas en cines'),
-        elevation: 0,
-        actions: [
-          IconButton(
-            onPressed: () => showSearch(context: context, delegate: MovieSearchDelegate()) , 
-            icon: Icon(Icons.search_outlined))
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-
-            // Tarjetas Principales
-            CardSwiper( 
-              movies: moviesProvider.onDisplayMovies
-            ),
-
-            // Slider de Películas
-            MovieSlider(
-              movies: moviesProvider.popularMovies,
-              title: 'Populares',
-              onNextPage: () => moviesProvider.getPopularMovies(), // opcional
-            ),
-
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Películas en Cines'),
+          elevation: 0,
+          actions: [
+            IconButton(
+                onPressed: () => showSearch(
+                    context: context, delegate: MovieSearchDelegate()),
+                icon: Icon(Icons.search_outlined))
           ],
-        ),      
-      )
-    );
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              // Tarjetas Principales
+              CardSwiper(movies: moviesProvider.onDisplayMovies),
+
+              // Slider de Películas
+              MovieSlider(
+                movies: moviesProvider.popularMovies,
+                title: 'Populares',
+                onNextPage: () => moviesProvider.getPopularMovies(), // opcional
+              ),
+            ],
+          ),
+        ));
   }
 }
