99ae353e80bda581fbb541606a49d2baaa14096a
Jhon Giraldo <jhon.giraldo@pragma.com.co>
2022-09-22 16:01:16 -0500

    Agregado efecto Hero en los MoviePosters

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -63,6 +63,7 @@ class _CustomAppBar extends StatelessWidget {
 }
 
 class _PostedAndTitle extends StatelessWidget {
+  
   final Movie movie;
 
   const _PostedAndTitle(this.movie);
@@ -77,13 +78,15 @@ class _PostedAndTitle extends StatelessWidget {
         padding: EdgeInsets.symmetric(horizontal: 20),
         child: Row(
           children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: FadeInImage(
-                placeholder: AssetImage('assets/no-image.jpg'),
-                image: NetworkImage(movie.fullPosterImg),
-                height: 250,
-                width: 180,
+            Hero(
+              tag: movie.heroId!,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: FadeInImage(
+                  placeholder: AssetImage('assets/no-image.jpg'),
+                  image: NetworkImage(movie.fullPosterImg),
+                  height: 250,
+                ),
               ),
             ),
             SizedBox(width: 20),
